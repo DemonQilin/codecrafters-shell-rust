907c994c93b5f1939c9d78ad1c20fe60a1f99f78
Demonqilin <velezloaizaesteban9012015@gmail.com>
2025-10-22 22:36:02 -0500

    feat: implement REPL loop for command input
    Add a loop to continuously read user input and display "command not found" error, creating a basic Read-Eval-Print Loop (REPL) functionality.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    // REPL
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
+        // Wait for user input
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
 
-    // Print error
-    println!("{}: command not found", command.trim());
+        // Print error
+        println!("{}: command not found", command.trim());
+    }
 }
