b0726a914932a3debfb134f4a59583e0c2999690
Demonqilin <velezloaizaesteban9012015@gmail.com>
2025-10-22 22:23:53 -0500

    feat: add command not found error message
    Add error handling to print "command not found" when an invalid command is entered.

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,7 @@ fn main() {
     // Wait for user input
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
+
+    // Print error
+    println!("{}: command not found", command.trim());
 }
